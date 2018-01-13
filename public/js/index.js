051a6c41fa6724922a37d81afb01db8e0db72954
erfexx <32482564+erfexx@users.noreply.github.com>
2018-01-13 22:42:51 +0700

    Add files via upload

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+// var anggrek, syahdan, type;
+// var body;
+
+// function onload(){
+// 	anggrek = document.getElementById("anggrek");
+// 	syahdan = document.getElementById("syahdan");
+// 	type = document.getElementById("type");
+
+// 	body = document.getElementById("app-layout");
+
+// 	anggrek.addEventListener("click", function(event){
+// 		//console.log();
+		
+// 		body.scrollTop = 1000;
+// 		console.log(body);
+// 		//event.preventDefault();
+// 	});
+// }
